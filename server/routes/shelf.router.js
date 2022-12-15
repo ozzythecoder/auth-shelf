8aac5a1e28a1dd11b25333210a78a380bf3351e5
Andrew Carey <andrew.r.carey@gmail.com>
2022-12-15 14:18:58 -0600

    mapping the shelf items onto the shelf

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const {rejectUnauthenticated,} = require('../modules/authentication-middleware');
+const {rejectUnauthenticated} = require('../modules/authentication-middleware');
 const pool = require('../modules/pool');
 const router = express.Router();
-const {rejectUnauthenticated} = require('../modules/authentication-middleware');
 
 
 /**
