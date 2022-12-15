a2b736e67c7be1ec3b2780bb526dacc38cfa884d
August McAllister <august.chm@gmail.com>
2022-12-15 11:51:48 -0600

    Input validation added, dispatch imported

--- src/components/AddToShelf/AddToShelf.jsx
+++ src/components/AddToShelf/AddToShelf.jsx
@@ -1,35 +1,46 @@
-// import e from "express";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 
+export default function AddToShelf() {
 
-export default function AddToShelf (){
+  const dispatch = useDispatch();
 
-    const [descriptionIn, setDescriptionIn] = useState('');
-    const [imageUrlIn, setImageUrlIn] = useState('');
+  const [descriptionIn, setDescriptionIn] = useState("");
+  const [imageUrlIn, setImageUrlIn] = useState("");
 
+  // user clicks submit button
+  function handleSubmit(event) {
+    event.preventDefault(); // prevent refresh
+    console.log("in handleSubmit!", descriptionIn, imageUrlIn);
 
-    function handleSubmit (event) {
-        event.preventDefault();
-        console.log('in handleSubmit!', descriptionIn, imageUrlIn);
-
+    // input validation
+    if(descriptionIn == '' || imageUrlIn == '') {
+      alert('All fields are required.')
+      return false;
     }
 
+    // dispatch data
+    
+  }
 
-    return (
-        <div className="addToShelf">
-            <p>ADD A NEW BOOK TO THE SHELF</p>
-            <form>
-                <input placeholder="Description" value={descriptionIn} onChange={event => setDescriptionIn(event.target.value)}/>
-                <input placeholder="Image URL" value={imageUrlIn} onChange={event => setImageUrlIn(event.target.value)}/>
-                <button type="submit" onClick={handleSubmit}>Submit Book</button>
-
-
-
-            </form>
-        </div>
-    )
-
-
-
-
+  return (
+    <div className="addToShelf">
+      <p>ADD A NEW BOOK TO THE SHELF</p>
+      <form>
+        <input
+          placeholder="Description"
+          value={descriptionIn}
+          onChange={(event) => setDescriptionIn(event.target.value)}
+        />
+        <input
+          placeholder="Image URL"
+          value={imageUrlIn}
+          onChange={(event) => setImageUrlIn(event.target.value)}
+        />
+        <button type="submit" onClick={handleSubmit}>
+          Submit Book
+        </button>
+      </form>
+    </div>
+  );
 }
