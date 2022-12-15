9f1ed65c039de53e0197afa56c3b6264416af6a8
Brett R. Gebbie <brg078@gmail.com>
2022-12-15 10:38:07 -0600

    inputaddtoshelf

--- src/components/AddToShelf/AddToShelf.jsx
+++ src/components/AddToShelf/AddToShelf.jsx
@@ -1,12 +1,28 @@
-
+// import e from "express";
+import { useState } from "react";
 
 
 export default function AddToShelf (){
 
+    const [descriptionIn, setDescriptionIn] = useState('');
+    const [imageUrlIn, setImageUrlIn] = useState('');
 
-    return(
 
-        <p>addtoshelf</p>
+    // function 
+
+
+    return (
+        <div className="addToShelf">
+            <p>ADD A NEW BOOK TO THE SHELF</p>
+            <form>
+                <input placeholder="Description" value={descriptionIn} onChange={event => setDescriptionIn(event.target.value)}/>
+                <input placeholder="Image URL" value={imageUrlIn} onChange={event => setImageUrlIn(event.target.value)}/>
+                <button type="submit">Submit Book</button>
+
+
+
+            </form>
+        </div>
     )
 
 
