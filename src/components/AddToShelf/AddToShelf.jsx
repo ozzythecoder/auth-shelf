e7e4c52e195c1603440d1514b3b0c0ee025aa806
August McAllister <august.chm@gmail.com>
2022-12-15 12:12:28 -0600

    Form submit is posting to the database, added comments

--- src/components/AddToShelf/AddToShelf.jsx
+++ src/components/AddToShelf/AddToShelf.jsx
@@ -1,10 +1,15 @@
+// import hooks
 import { useState } from "react";
 import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 export default function AddToShelf() {
 
+  // initialize hooks
   const dispatch = useDispatch();
+  const history = useHistory();
 
+  // local state variables
   const [descriptionIn, setDescriptionIn] = useState("");
   const [imageUrlIn, setImageUrlIn] = useState("");
 
@@ -20,25 +25,43 @@ export default function AddToShelf() {
     }
 
     // dispatch data
+    dispatch({
+      type: 'ADD_NEW_ITEM',
+      payload: {
+        description: descriptionIn,
+        image_url: imageUrlIn
+      }
+    })
+
+    // clear inputs
+    setDescriptionIn('');
+    setImageUrlIn('');
     
+    // move user to shelf
+    history.push('/shelf');
   }
 
   return (
     <div className="addToShelf">
-      <p>ADD A NEW BOOK TO THE SHELF</p>
+      <h2>ADD A NEW ITEM TO THE SHELF</h2>
+      
       <form>
+        
+        {/* Description Input */}
         <input
           placeholder="Description"
           value={descriptionIn}
           onChange={(event) => setDescriptionIn(event.target.value)}
         />
+
+        {/* Image URL Input */}
         <input
           placeholder="Image URL"
           value={imageUrlIn}
           onChange={(event) => setImageUrlIn(event.target.value)}
         />
         <button type="submit" onClick={handleSubmit}>
-          Submit Book
+          Add Item
         </button>
       </form>
     </div>
